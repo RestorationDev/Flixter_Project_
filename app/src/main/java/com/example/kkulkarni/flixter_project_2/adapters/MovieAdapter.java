2e921b564027b986203397f10abd9e688ee22c1b
RestorationDev <4D4Kunal@gmail.com>
2021-09-23 22:41:26 -0700

    New commit with a youtube trailer preview feature

--- app/src/main/java/com/example/kkulkarni/flixter_project_2/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/kkulkarni/flixter_project_2/adapters/MovieAdapter.java
@@ -1,20 +1,24 @@
 package com.example.kkulkarni.flixter_project_2.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.example.kkulkarni.flixter_project_2.MainActivity;
+import com.example.kkulkarni.flixter_project_2.DetailActivity;
 import com.example.kkulkarni.flixter_project_2.R;
 import com.example.kkulkarni.flixter_project_2.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -52,7 +56,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -60,18 +64,25 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = (itemView.findViewById(R.id.tvTitle));
-            tvOverview = (itemView.findViewById(R.id.tvOverview));
+            tvOverview = (itemView.findViewById(R.id.tvOvrvw));
             ivPoster = (itemView.findViewById(R.id.ivPoster));
-
-
-
+            container = (itemView.findViewById(R.id.container));
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
-
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                // 1. Reg click container on the whole row
+                // 2. Navigate to new activity on tap
+                public void onClick(View view) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/kkulkarni/flixter_project_2/models/Movie.java
+++ app/src/main/java/com/example/kkulkarni/flixter_project_2/models/Movie.java
@@ -3,19 +3,32 @@ package com.example.kkulkarni.flixter_project_2.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Parcel
 public class Movie{
+    int movieId;
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+    //needed for parceller library
+    public Movie(){
 
+    }
     public Movie (JSONObject jsonObject) throws JSONException {
+
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
 
 
     }
@@ -31,6 +44,10 @@ public class Movie{
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
+    public String getBackdropPath(){
+        return String.format("https://image.tmdb.org/t/p/w342/%s",backdropPath);
+    }
+
     public String getTitle() {
         return title;
     }
@@ -38,4 +55,8 @@ public class Movie{
     public String getOverview() {
         return overview;
     }
+
+    public double getRating(){return rating;}
+
+    public int getMovieId(){return movieId;}
 }
